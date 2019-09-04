3f12a96f28140ece05786292226de13e6f0c3a29
imyhui <1359158019@qq.com>
2019-09-04 22:10:14 +0800

    feat: add version info

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
+	"os"
 	"time"
 
 	"rest-api/config"
@@ -14,14 +17,28 @@ import (
 	"github.com/lexkong/log"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+	v "rest-api/pkg/version"
 )
 
 var (
-	cfg = pflag.StringP("config", "c", "", "rest-api config file path")
+	cfg     = pflag.StringP("config", "c", "", "rest-api config file path")
+	version = pflag.BoolP("version", "v", false, "show version info.")
 )
 
 func main() {
 	pflag.Parse()
+
+	if *version {
+		v := v.Get()
+		marshalled, err := json.MarshalIndent(&v, "", "  ")
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(string(marshalled))
+		return
+	}
 
 	// init config
 	if err := config.Init(*cfg); err != nil {

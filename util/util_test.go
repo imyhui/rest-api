5bed645bbdad84893aac843a29022bbfd2e7eb9e
imyhui <1359158019@qq.com>
2019-09-17 16:41:00 +0800

    feat: add test for util

---
+++ util/util_test.go
@@ -0,0 +1,37 @@
+package util
+
+import "testing"
+
+// Unit Test
+func  TestGenShortId(t *testing.T) {
+	shortId, err := GenShortId()
+	if shortId == "" || err != nil {
+		t.Error("GenShortId failed!")
+	}
+
+	t.Log("GenShortId test pass")
+}
+
+
+// Performance Test
+func BenchmarkGenShortId(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		GenShortId()
+	}
+}
+
+func BenchmarkGenShortIdTimeConsuming(b *testing.B)  {
+	b.StopTimer() 	// Stop counting time
+
+	shortId, err := GenShortId()
+	if shortId == "" || err != nil {
+		b.Error(err)
+	}
+
+	b.StartTimer() 	// Restart counting time
+
+	for i := 0; i < b.N; i++ {
+		GenShortId()
+	}
+}
+
